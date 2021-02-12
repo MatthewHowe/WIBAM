cd01a9f1f99f59770eb696f806f053c8e73e67fc
Matthew Howe <matthew@aiml.team>
2021-02-12 14:56:10 +1030

    adding option to turn off validation for debugging

--- src/main_mixed-train.py
+++ src/main_mixed-train.py
@@ -135,15 +135,16 @@ def main(opt):
     mark = epoch if opt.save_all else 'last'
 
     # Do a round of validation
-    save_model(os.path.join(opt.save_dir, 'model_{}.pth'.format(mark)), 
-                epoch, model, optimizer)     
-    with torch.no_grad():
-      log_dict_val, preds = trainer.val(epoch, val_loader)
-      if opt.eval_val:
-        val_loader.dataset.run_eval(preds, opt.save_dir)
-    for k, v in log_dict_val.items():
-      logger.scalar_summary('val_{}'.format(k), v, epoch)
-      logger.write('{} {:8f} | '.format(k, v))
+    if opt.val_intervals > 0:
+      save_model(os.path.join(opt.save_dir, 'model_{}.pth'.format(mark)), 
+                  epoch, model, optimizer)     
+      with torch.no_grad():
+        log_dict_val, preds = trainer.val(epoch, val_loader)
+        if opt.eval_val:
+          val_loader.dataset.run_eval(preds, opt.save_dir)
+      for k, v in log_dict_val.items():
+        logger.scalar_summary('val_{}'.format(k), v, epoch)
+        logger.write('{} {:8f} | '.format(k, v))
 
     # Complete training on second dataset
     for _ in range(opt.mixed_schedule[1]):
@@ -178,15 +179,16 @@ def main(opt):
       finished = epoch >= opt.num_epochs
     
     # Complete a validation set to see improvement
-    save_model(os.path.join(opt.save_dir, 'model_{}.pth'.format(mark)), 
-                epoch, model, optimizer)     
-    with torch.no_grad():
-      log_dict_val, preds = trainer.val(epoch, val_loader)
-      if opt.eval_val:
-        val_loader.dataset.run_eval(preds, opt.save_dir)
-    for k, v in log_dict_val.items():
-      logger.scalar_summary('val_{}'.format(k), v, epoch)
-      logger.write('{} {:8f} | '.format(k, v))
+    if opt.val_intervals > 0:
+      save_model(os.path.join(opt.save_dir, 'model_{}.pth'.format(mark)), 
+                  epoch, model, optimizer)     
+      with torch.no_grad():
+        log_dict_val, preds = trainer.val(epoch, val_loader)
+        if opt.eval_val:
+          val_loader.dataset.run_eval(preds, opt.save_dir)
+      for k, v in log_dict_val.items():
+        logger.scalar_summary('val_{}'.format(k), v, epoch)
+        logger.write('{} {:8f} | '.format(k, v))
 
     # Complete training on first dataset
     for _ in range(opt.mixed_schedule[0]):
