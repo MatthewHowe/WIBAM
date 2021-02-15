0bfef01bbc8dec88d888cd8447bf2f470a883aa9
Matthew Howe <matthew@aiml.team>
2021-02-15 15:51:14 +1030

    Using profiler for training

--- src/lib/model/mv_losses.py
+++ src/lib/model/mv_losses.py
@@ -241,8 +241,7 @@ class ReprojectionLoss(nn.Module):
 
     # Make sure that number of detections is equal to number of gt detections
     if 'det' in pr_dict:  
-      # mv_loss['mult'] = pow((torch.sum(batch['mask_det']) - len(pr_dict['det'])),2) + 1.
-      mv_loss['mult'] = 1
+      mv_loss['mult'] = pow((torch.sum(batch['mask_det']) - len(pr_dict['det'])),2) + 1.
     else:
       mv_loss['mult'] = pow((torch.sum(batch['mask_det']) - 0),2) + 1.
     mv_loss['tot_GIoU'] = mv_loss['tot']
