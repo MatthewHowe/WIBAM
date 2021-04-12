0301067bb6a0342bcb8c60542239347f3bc21823
Matthew Howe <matthew@aiml.team>
2021-04-12 14:22:10 +0930

    max step and completion criterior adjustment

--- src/main_lit.py
+++ src/main_lit.py
@@ -310,8 +310,8 @@ if __name__ == '__main__':
 						 gpus=opt.gpus, accelerator="dp",
 						 check_val_every_n_epoch=2,
 						 plugins=[my_ddp],
-						 max_steps=6000
+						 max_steps=10000
 						 )
 
-	# trainer.test(model)
+	trainer.test(model)
 	trainer.fit(model)
