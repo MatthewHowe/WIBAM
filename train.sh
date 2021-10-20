3d2cebaf627d35e4aaa1a8bf197b4d0535855353
Matthew Howe <matthew@aiml.team>
2021-10-20 12:02:38 +1030

    Removing unnecessary files and folders

--- train.sh
+++
@@ -1,8 +0,0 @@
-export GOOGLE_APPLICATION_CREDENTIALS="/home/matt/WIBAM/WIBAM/data/data-manager.json"
-python src/main_lit.py ddd --load_model=data/models/nuScenes_3Ddetection_e140.pth \
-                --dataset=nuscenes --batch_size=80 \
-                --mixed_dataset=wibam --mixed_batchsize=12 \
-                --gpus=0,1,2,3 --num_workers=12 \
-                --lr=4.48e-6 \
-                --output_path="gs://aiml-reid-casr-data/lightning_experiments"
-exit 0
